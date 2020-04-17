905cb9e043d15d040d8142d8497541c78b473f54
Peter Laffey <peter.laffey@gmail.com>
2020-04-17 14:16:54 -0500

    Add modal code along

--- au-modal/src/app/au-modal/au-modal.module.ts
+++ au-modal/src/app/au-modal/au-modal.module.ts
@@ -4,24 +4,27 @@
 
 import {ModuleWithProviders, NgModule} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuModalComponent } from './au-modal.component';
+import { AuModalOpenOnClickDirective } from './au-modal-open-on-click.directive';
+import { AuModalService } from './modal.service';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AuModalComponent, AuModalOpenOnClickDirective],
   imports: [
     CommonModule
   ],
-  exports: []
+  exports: [AuModalComponent, AuModalOpenOnClickDirective]
 })
 export class AuModalModule {
 
-
-
-
-
-
-
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: AuModalModule, 
+      providers: [AuModalService]
+    }
+  }
 
 
 }
